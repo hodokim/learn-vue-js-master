26034463ad82c2e2abdc0b08bf9c30b71814c7fa
hodokim <hodo323@naver.com>
2021-09-16 23:47:42 +0900

    Vue Study Day 16

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    
+    <app-header v-bind:propsdata="str"></app-header>
   </div>
 </template>
 
 <script>
-export default {
+import AppHeader from './components/AppHeader.vue';
 
+export default {
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
+  components:{
+    'app-header': AppHeader  
+  }
 }
 </script>
 
--- vue-cli/src/a.vue
+++ vue-cli/src/a.vue
@@ -1,10 +1,15 @@
 <template>
-  
+  <div>header</div>
 </template>
 
 <script>
 export default {
+    // Javascript - 인스턴스 옵션
+    methods: {
+        addNum: function(){
 
+        }
+    }
 }
 </script>
 
